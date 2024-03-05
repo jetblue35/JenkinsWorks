ca23f0ece1059721322b819ae16cf68c888f443c
Muhammet Emrah KÜÇÜK <muhammet.kucuk@tubitak.gov.tr>
2024-03-05 14:51:23 +0300

    Student.h dosyasına eklemeler yapıldı.

--- Student.h
+++ Student.h
@@ -9,6 +9,9 @@ public:
 
 Student::Student(/* args */)
 {
+    // Print out the name and age of the student
+    std::cout << "Name: " << name << std::endl;
+    std::cout << "Age: " << age << std::endl;
 }
 
 Student::~Student()
