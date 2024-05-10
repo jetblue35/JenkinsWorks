32d3c7928fae5727bc2bf94343ef7d529055e81d
Muhammet Emrah KÜÇÜK <muhammet.kucuk@tubitak.gov.tr>
2024-05-10 14:41:54 +0300

    Main.cpp has been updated.

--- Main.cpp
+++ Main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char const *argv[])
     People person1("John", 21);
     person1.printInfo();
     
-    std::cout<< "Emrah yeter yedin.."<<std::endl;
+    std::cout<< "People1"<<std::endl;
+    std::cout<< "People2"<<std::endl;
     return 0;
 }
