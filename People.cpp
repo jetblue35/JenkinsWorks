adc16d45bdbcd8ebf9fa8b728d9648457e3a96c5
Muhammet Emrah KÜÇÜK <muhammet.kucuk@tubitak.gov.tr>
2024-05-13 15:04:00 +0300

    Update People.cpp file

--- People.cpp
+++ People.cpp
@@ -13,6 +13,7 @@ void People::printInfo()
 {
     std::cout << "Name: " << name << std::endl;
     std::cout << "Age: " << age << std::endl;
+    std::cout << "----------------" << std::endl;
 }
 void People::setAge(int age)
 {
